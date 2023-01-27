8724e305dcbfc468fe896ef67470f9eb66c1e7af
yuechuc <304723403@qq.com>
2023-01-27 14:27:55 +0800

    微服务远程调用，利用RestTemplate发起HTTP请求

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
@@ -13,7 +14,14 @@ public class OrderApplication {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+    /**
+     * 创建RestTemplate并注入spring容器
+     * @return
+     */
     @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    };
 
 
 }
